e8fa72cbfb5c6d96c08079b077bd9e9d9f669d05
root <root@kvm20488.ddos-guard.net>
2019-12-24 01:23:36 +0300

    Updates

---
+++ precache-manifest.0e6eb74f89d35f08a9e811eb1f84fb9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d832e56e97f52900e583750c5de114cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "63f70f322771ac8b2e5b",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "b3bb97bc88bbade231d5",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "63f70f322771ac8b2e5b",
+    "url": "./static/js/2.0d7dc8bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.0d7dc8bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3bb97bc88bbade231d5",
+    "url": "./static/js/main.edf24c41.chunk.js"
+  },
+  {
+    "revision": "c405d80961b2171c540d",
+    "url": "./static/js/runtime-main.48641bc6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83fd370930e0019af2ce8720ca41b9b1.js"
+  "./precache-manifest.0e6eb74f89d35f08a9e811eb1f84fb9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
