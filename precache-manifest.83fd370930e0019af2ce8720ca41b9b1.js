320f5b8b1ff66d04f966d09344abbba695d63d54
root <root@kvm20488.ddos-guard.net>
2019-12-24 01:22:00 +0300

    Updates

---
+++ precache-manifest.83fd370930e0019af2ce8720ca41b9b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c17de65c3ad7086b07b9c35f6b452fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37de5f2fc850647f13ed",
+    "url": "/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "b3bb97bc88bbade231d5",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "37de5f2fc850647f13ed",
+    "url": "/static/js/2.83f7a1b7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.83f7a1b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3bb97bc88bbade231d5",
+    "url": "/static/js/main.edf24c41.chunk.js"
+  },
+  {
+    "revision": "821ce30dfc21b86bfe6b",
+    "url": "/static/js/runtime-main.b29af7db.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
